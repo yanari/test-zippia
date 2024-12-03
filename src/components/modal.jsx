4f7fd5b22a34eb25c01cab95d98e7a61adbee690
Marcelle Yanari <yanarimy@gmail.com>
2024-12-02 22:54:33 -0300

    modal content customization

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -12,7 +12,7 @@ export default function Modal() {
         <>
             <div className="fixed top-0 bottom-0 left-0 right-0 bg-black/70" onClick={handleCloseModal}/>
             <div className="rounded-md w-2/4 fixed -translate-x-1/2 -translate-y-1/2 z-20 bg-neutral-50 top-1/2 left-1/2">
-                <UserItem data={content} />
+                <UserItem data={content} isComplete />
             </div>
         </>,
         document.getElementById('portal')
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,38 +1,59 @@
-export default function UserItem({ data }) {
+export default function UserItem({ data, isComplete }) {
     const {
         name,
         username,
         email,
-        address: { city },
+        address: { city, street, suite },
+        website,
         phone,
         company,
     } = data;
 
+    const fontSizes = {
+        nameFontSize: isComplete ? 'text-2xl' : 'text-xl',
+        infoFontSize: isComplete ? 'text-md' : 'text-sm',
+        companyName: isComplete ? 'text-xl' : 'text-lg',
+        companyInfo: isComplete ? 'text-md' : 'text-sm',
+    }
+
     return (
         <li className="text-start rounded-md shadow-md p-4 bg-white grid gap-4">
             <div className="flex gap-2 items-baseline grow">
-                <h3 className="font-semibold text-xl">{name}</h3>
+                <h3 className={`font-semibold ${fontSizes.nameFontSize}`}>{name}</h3>
                 <span className="text-sm text-gray-600">@{username}</span>
             </div>
             <div>
-                <p className="text-sm">
+                <p className={fontSizes.infoFontSize}>
                     Email:{" "}
                     <span className="text-gray-500">{email}</span>
                 </p>
-                <p className="text-sm">
+                <p className={fontSizes.infoFontSize}>
                     Phone:{" "}
                     <span className="text-gray-500">{phone}</span>
                 </p>
-                <p className="text-sm">
+                <p className={fontSizes.infoFontSize}>
                     City:{" "}
                     <span className="text-gray-500">{city}</span>
                 </p>
+                {isComplete && (
+                    <>
+                        <p className="text-md">
+                            Street:{" "}
+                            <span className="text-gray-500">{street}, {suite}</span>
+                        </p>
+                    </>
+                )}
             </div>
+            {isComplete && (
+                <div className="text-sm text-center text-gray-500">
+                    <a className="underline text-teal-700 text-xl italic" href={website}>{website}</a>
+                </div>
+            )}
             <hr />
             <div className="text-end">
-                <h4 className="text-teal-700 font-semibold text-lg">{company?.name}</h4>
-                <p className="italic text-sm">{company?.catchPhrase}</p>
-                <p className="text-gray-500 text-sm">{company?.bs}</p>
+                <h4 className={`text-teal-700 font-semibold ${fontSizes.companyName}`}>{company?.name}</h4>
+                <p className={`italic ${fontSizes.companyInfo}`}>{company?.catchPhrase}</p>
+                <p className={`text-gray-500 ${fontSizes.companyInfo}`}>{company?.bs}</p>
             </div>
         </li>
     )
